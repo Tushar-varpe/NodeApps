f62c7cd20951ce3f9cb0590c7ab87e4fc10638e8
Tushar-varpe <tusharvarpe333@gmail.com>
2026-05-13 23:12:13 +0530

    First commit

---
+++ 03_ConversionOperation.js
@@ -0,0 +1,52 @@
+let score =undefined
+//console.log(typeof score);
+let valueInNumber = Number(score)
+//console.log( typeof valueInNumber)
+//console.log(  valueInNumber)
+
+   // "33"=33
+   // "33abc"=NaN
+   // true=1 ; false=0
+   // null=0
+   // undefined=NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+   // 1=true 0=false 
+   // ""=false 
+   // "Tushar"=True
+
+let someNumber = 31
+let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+// ***********************OPERATION****************************
+
+let value =5
+let negvalue = -value
+//console.log(negvalue)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello"
+let str2 = " Tushar"
+let str3 =str1+str2
+// console.log(str3)
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
